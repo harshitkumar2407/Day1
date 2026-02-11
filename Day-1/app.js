ff66e42370f6c9353992c73dab2817d83ec07481
harshitkumar2407 <140981179+harshitkumar2407@users.noreply.github.com>
2026-02-11 21:57:07 +0530

    first commit

---
+++ Day-1/app.js
@@ -0,0 +1,20 @@
+// const catMe = require("cat-me")
+// import catMe from "cat-me";
+// import {randomDogName, randomFemaleDogName} from 'dog-names';
+
+// // console.log(catMe());
+// console.log(randomDogName());
+const express = require("express")
+
+const app = express() // server instance areate krna
+app.get('/',(req,res) => {
+    res.send("Helllo world")
+})
+app.get('/hi',(req,res) => {
+    res.send("Helllo ")
+})
+app.get('/hii',(req,res) => {
+    res.send("nikal lode")
+})
+
+app.listen(3000) // server start krna
