bc5e99d1aafbe7416aa3a8630fb718c5d0b6d0d5
harshit Kumar <harshitkumar2408@gmail.com>
2026-03-14 20:17:59 +0530

    login & Register connected to backend

--- Day-19/Frontend/src/features/auth/pages/Login.jsx
+++ Day-19/Frontend/src/features/auth/pages/Login.jsx
@@ -1,14 +1,39 @@
+import { useState } from "react"
 import "../style/form.scss"
 import {Link} from 'react-router'
+import axios from "axios"
 
 const Login = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    axios.post("http://localhost:3000/api/auth/login",{
+      username,password
+    },{
+      withCredentials:true
+    }).then(res =>{
+      console.log(res.data);
+      
+    })
+  }
   return (
     <main>
       <div className="form-container">
         <h1>Login</h1>
-        <form action="">
-          <input type="text" name="username" placeholder='Enter username' />
-          <input type="password" name="password" placeholder='Enter password' />
+          <form onSubmit={handleSubmit}>
+            <input 
+              type="text" 
+              name="username" 
+              placeholder='Enter username'
+              onInput={(e)=>{setUsername(e.target.value)}} />
+          <input 
+            type="password" 
+            name="password" 
+            placeholder='Enter password'
+            onInput={(e) => {setPassword(e.target.value)}} />
           <button >Login </button>
         </form>
         <p>If you don't have a account ? <Link className="toggleAuthForm" to={"/register"}>Register</Link></p>
